0b98631de8aa3f739aec43feb7f8ccaee2950489
totarochristian <totaro.christian.18@gmail.com>
2023-10-22 15:12:44 +0200

    Add prediction class based on the response of the http request

---
+++ LandmarkAI/Classes/Prediction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandmarkAI.Classes
+{
+    public class Prediction
+    {
+        public string TagId { get; set; }
+        public string Tag { get; set; }
+        public double Probability { get; set; }
+    }
+}
