dea6fdc266d21a01a4c0e2d2240c4e44cdc49eab
totarochristian <totaro.christian.18@gmail.com>
2023-10-22 15:13:03 +0200

    Add custom vision class based on the response of the http request

---
+++ LandmarkAI/Classes/CustomVision.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandmarkAI.Classes
+{
+    public class CustomVision
+    {
+        public string Id { get; set; }
+        public string Project { get; set; }
+        public string Iteration { get; set; }
+        public DateTime Created { get; set; }
+        public IList<Prediction> Predictions { get; set; }
+    }
+}
