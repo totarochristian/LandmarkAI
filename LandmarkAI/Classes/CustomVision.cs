042f3a3a99a458ffe982a5584ffbfe0a54a0e549
totarochristian <totaro.christian.18@gmail.com>
2023-10-22 15:19:40 +0200

    Update CustomVision class using List instead of IList

--- LandmarkAI/Classes/CustomVision.cs
+++ LandmarkAI/Classes/CustomVision.cs
@@ -12,6 +12,6 @@ namespace LandmarkAI.Classes
         public string Project { get; set; }
         public string Iteration { get; set; }
         public DateTime Created { get; set; }
-        public IList<Prediction> Predictions { get; set; }
+        public List<Prediction> Predictions { get; set; }
     }
 }
