a514b125ecbc811d203faf04ac9dda76956e8505
totarochristian <totaro.christian.18@gmail.com>
2023-10-22 14:11:17 +0200

    Update select image button adding name and click event in the main window

--- LandmarkAI/MainWindow.xaml.cs
+++ LandmarkAI/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace LandmarkAI
         {
             InitializeComponent();
         }
+
+        private void SelectImageButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
